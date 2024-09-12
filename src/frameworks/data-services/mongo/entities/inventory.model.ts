46e3f8205b3ec87b6cf0eedb0ebac6bbbc1a1805
Yuri Cordeiro <yuricordeiroc@hotmail.com>
2024-09-12 20:37:36 -0300

    Change object mapping

--- src/frameworks/data-services/mongo/entities/inventory.model.ts
+++ src/frameworks/data-services/mongo/entities/inventory.model.ts
@@ -1,17 +1,11 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Product } from "./product.model";
-import mongoose, { ObjectId } from "mongoose";
 
 export type InventoryDocument = Inventory & Document;
 
 @Schema()
 export class Inventory {
-
     id: string
-
-    @Prop({
-        type: mongoose.Schema.Types.ObjectId
-    })
     product: Product;
     @Prop()
     totalAvailable: number;
